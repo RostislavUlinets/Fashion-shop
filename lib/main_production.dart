af5d4f1a9254cf1df11bbda23f7321a108816de8
Rostislav <56642590+RostislavUlinets@users.noreply.github.com>
2022-07-30 09:45:16 +0300

    first commit

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:fashion_shop/app_bloc_observer.dart';
+import 'package:fashion_shop/my_app.dart';
+
+void main() {
+  bootstrap(MyApp.new);
+}
